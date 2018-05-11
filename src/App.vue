24f21c76d9129a23935f8b3ff8c0af39e4da5285
james <james20209@msn.com>
2018-05-11 12:21:37 +0800

    d

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="menu">
+      <router-link to="/about">about</router-link>
+      <router-link to="/product">product</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import VueRouter from 'vue-router'
+  import Product from './product.vue'
+  import About from './about.vue'
+
+  export default {
+    router : new VueRouter({
+      routes:[
+        {path:'/about',component:About},
+        {path:'/product',component:Product}
+      ]
+    })
+  }
+</script>
---
+++ src/product.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>product</h1>
+</template>
